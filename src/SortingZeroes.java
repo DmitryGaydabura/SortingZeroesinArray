d3c11851f233ac624cd3080e6b0ad56f5486c0bd
DmitryGaydabura <gaydabura.d@icloud.com>
2022-05-17 00:25:51 +0300

    Bug fixed

--- src/SortingZeroes.java
+++ src/SortingZeroes.java
@@ -4,12 +4,19 @@ import java.util.Arrays;
 public class SortingZeroes {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        int[] array = {-1, 2, 0, 4, 5, 6, 0, 0, 0};
+        int[] array = new int[100000]; /*= {-1, 2, 0, 4, 5, 6, 0, 0, 0};*/
+        fillArray(array);
         System.out.println("Your array is:        " + Arrays.toString(array));
         System.out.println("Your sorted array is: " + Arrays.toString(putZeroesInPlace(array)));
         long end = System.currentTimeMillis();
         System.out.println("Sorting Time: " + (end - start));
 
+    }
+
+    public static void fillArray(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int)(Math.random()*3);
+        }
     }
 
     public static int[] putZeroesInPlace(int[] array) {
